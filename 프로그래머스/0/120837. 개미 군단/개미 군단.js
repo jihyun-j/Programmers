b177677babf27718df581ae3e800ebd02a7cfd09
Jihyun Jeon <80092348+jihyun-j@users.noreply.github.com>
2024-11-14 22:32:44 +0900

    [level 0] Title: 개미 군단, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120837. 개미 군단/개미 군단.js
@@ -0,0 +1,7 @@
+function solution(hp) {
+    const first = Math.floor(hp / 5) 
+    const second = Math.floor((hp % 5) / 3 )
+    const third = (hp % 5) % 3
+    
+    return first + second + third
+}
